b8447dbdf47787182c91e6afc1e9ddd49af3f766
yyyzzq <742984152@qq.com>
2019-08-12 16:09:31 +0800

    修改初始化方式

---
+++ Example/MyRefresh/MyRefreshTestHeader.swift
@@ -0,0 +1,30 @@
+//
+//  MyRefreshTestHeader.swift
+//  MyRefresh_Example
+//
+//  Created by yyyzzq on 2019/8/12.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import MyRefresh
+
+class MyRefreshTestHeader: MyRefreshHeader {
+
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Example/MyRefresh/ViewController.swift
+++ Example/MyRefresh/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import MyRefresh
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(refreshHeader: MyRefreshTestHeader()) {
+            
+        }
+        view.addSubview(tableView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
 
